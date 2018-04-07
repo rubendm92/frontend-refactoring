f24cbd11e2b9cea7a4b5587b9d10c68f4a4f53f8
Rubén Díaz Martínez <rubendm92@gmail.com>
2018-04-07 17:21:26 +0100

    Refactor to make test more expressive

---
+++ e2e_tests/Browser.js
@@ -0,0 +1,39 @@
+const puppeteer = require('puppeteer');
+const expect = require('chai').expect;
+
+const fill = page => (selector, value) => page.type(selector, value);
+
+const click = page => selector => page.click(selector);
+
+const elementPresent = page => async selector => {
+    const timeout = 500;
+    try {
+        await page.waitForSelector(selector, {timeout: timeout});
+    } catch (e) {
+        throw new Error(`Element "${selector}" not found after ${timeout} ms`);
+    }
+};
+
+const elementHasText = page => async (selector, expectedText) => {
+    elementPresent(page)(selector);
+    const actualText = await page.evaluate(selector => document.querySelector(selector).textContent, selector);
+    expect(expectedText).to.equal(actualText);
+};
+
+module.exports = url => fn => (async () => {
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    try {
+        await fn({
+            fill: fill(page),
+            click: click(page),
+            elementPresent: elementPresent(page),
+            elementHasText: elementHasText(page)
+        });
+    } finally {
+        await browser.close();
+    }
+});
---
+++ e2e_tests/NoteBook.js
@@ -0,0 +1,18 @@
+class NoteBook {
+    constructor(page) {
+        this._page = page;
+    }
+
+    async publishNote(text, date) {
+        await this._page.fill('#note_input', text);
+        await this._page.fill('#date_input', date);
+        await this._page.click('input[type="submit"]');
+    };
+
+    async noteExists(content) {
+        await this._page.elementPresent('li');
+        await this._page.elementHasText('li > p', content);
+    };
+}
+
+module.exports = NoteBook;

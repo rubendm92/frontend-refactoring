ef27200900845730db74013eb181af43aa29874a
Rubén Díaz Martínez <rubendm92@gmail.com>
2018-04-08 10:39:22 +0100

    Refactor actionsSpec

--- tests/unit/ActionsSpec.js
+++ tests/unit/ActionsSpec.js
@@ -2,52 +2,45 @@ const expect = require('chai').expect;
 const {addNote} = require('../../js/actions');
 
 describe('Actions', () => {
+
     describe('Add note', () => {
-        it('should add note to the repository', function () {
-            const repository = (() => {
+
+        let repository;
+
+        beforeEach(() => {
+            repository = (() => {
                 const notes = [];
                 return {
                     add: notes.push.bind(notes),
-                    notes: () => notes
+                    get: () => notes
                 }
             })();
+        });
+
+        it('should add note to the repository', function () {
             const f = addNote(repository);
 
             f({text: 'hello', date: '1992-06-22'});
 
-            expect(repository.notes()).to.deep.equal([
+            expect(repository.get()).to.deep.equal([
                 {text: 'hello', date: '1992-06-22'}
             ]);
         });
 
         it('should not add a note without text', function () {
-            const repository = (() => {
-                const notes = [];
-                return {
-                    add: notes.push.bind(notes),
-                    notes: () => notes
-                }
-            })();
             const f = addNote(repository);
 
             f({text: '', date: '1992-06-22'});
 
-            expect(repository.notes()).to.be.empty;
+            expect(repository.get()).to.be.empty;
         });
 
         it('should not add a note without date', function () {
-            const repository = (() => {
-                const notes = [];
-                return {
-                    add: notes.push.bind(notes),
-                    notes: () => notes
-                }
-            })();
             const f = addNote(repository);
 
             f({text: 'hello'});
 
-            expect(repository.notes()).to.be.empty;
+            expect(repository.get()).to.be.empty;
         });
     });
 });

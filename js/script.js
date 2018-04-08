cbd2a16addf2af006b9693d955b1c00de1b229f5
Rubén Díaz Martínez <rubendm92@gmail.com>
2018-04-08 12:12:17 +0100

    Simplify using events

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-const persistence = repository(localStorage);
-
-const reload = notes => showNotes(notes, removeNote(persistence, reload));
-const add = addNote(persistence, reload);
-reload(persistence.get());
-
-document.querySelector('form').addEventListener('submit', e => {
-    e.preventDefault();
-    add(retrieveInput());
-});
+(repository => {
+    const add = addNote(repository, showNotes);
+    const remove = removeNote(repository, showNotes);
+    showNotes(repository.get());
+    document.body.addEventListener('add-note', e => add(e.detail.note));
+    document.body.addEventListener('remove-note', e => remove(e.detail.note));
+})(repository(localStorage));
--- js/view.js
+++ js/view.js
@@ -5,24 +5,31 @@ const retrieveInput = () => {
     };
 };
 
-const showNotes = (notes, removeNote) => {
+const showNotes = notes => {
     document.getElementById('notes').innerHTML = '';
-    notes.forEach(n => showNote(n, removeNote));
+    notes.forEach(showNote);
 };
 
-const showNote = (note, removeNote) => {
+const showNote = note => {
     const notes = document.getElementById('notes');
     const noteElement = document.createElement('li');
     const content = document.createElement('p');
     content.textContent = `${note.text} ${note.date}`;
     noteElement.appendChild(content);
-    noteElement.appendChild(deleteButton(note, removeNote));
+    noteElement.appendChild(deleteButton(note));
     notes.appendChild(noteElement);
 };
 
-const deleteButton = (note, removeNote) => {
+const deleteButton = note => {
     const button = document.createElement('button');
     button.textContent = 'Borrar';
-    button.addEventListener('click', () => removeNote(note));
+    button.addEventListener('click', () => {
+        button.dispatchEvent(new CustomEvent('remove-note', { bubbles: true, detail: { note: note }}));
+    });
     return button;
 };
+
+document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+    document.body.dispatchEvent(new CustomEvent('add-note', { bubbles: true, detail: { note: retrieveInput() } }));
+});

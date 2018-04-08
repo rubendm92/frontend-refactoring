a0a57d301ea66ac89666cc04f6fbe903f3d0ebee
Rubén Díaz Martínez <rubendm92@gmail.com>
2018-04-08 18:29:51 +0100

    Clean up

--- tests/integration/trick.js
+++ tests/integration/trick.js
@@ -1,3 +1,4 @@
+// This is just a trick to make repository global for NotesRepositorySpec
 import repository from '../../js/persistence';
 
 window.repository = repository;
--- webpack.config.js
+++ webpack.config.js
@@ -20,15 +20,9 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [
-                    {
-                        loader: "style-loader" // creates style nodes from JS strings
-                    },
-                    {
-                        loader: "css-loader" // translates CSS into CommonJS
-                    },
-                    {
-                        loader: "sass-loader" // compiles Sass to CSS
-                    }
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "sass-loader" }
                 ]
             }
         ]
